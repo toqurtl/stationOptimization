292e45cffeb67cb44649d554c734ce55bda62217
toqurtl <39524978+toqurtl@users.noreply.github.com>
2020-02-10 20:43:00 +0900

    first commit

--- modular_io/read_factory.py
+++ modular_io/read_factory.py
@@ -1,4 +1,4 @@
-
+from component.activity import Activity
 
 class temp_activity():
     def __init__(self, line_list):
@@ -7,19 +7,26 @@ class temp_activity():
         self.manhour = int(line_list[2])
         self.labor_type = str(line_list[3])
         self.fixed = bool(line_list[5])
+        self.num_labor = 0
         if self.fixed:
             self.num_labor = int(line_list[4])
         self.location = str(line_list[6])
         self.max_labor = int(line_list[7])
 
+    def print_information(self):
+        print(str(self.id), str(self.name))
+
 def read_file(filename):
+    temp_list = []
     with open(filename, 'r') as f:
         lines = f.readlines()
         for i, line in enumerate(lines):
             if i is not 0:
-                line.split(',')
-
-
+                temp_list.append(line.split(','))
+    return temp_list
 
 if __name__=='__main__':
-    read_file('../modular3.csv')
+    temp_list = read_file('../productionline.csv')
+    activity_list = []
+    for temp in temp_list:
+        activity_list.append(temp_activity(temp))
