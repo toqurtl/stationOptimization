41a462739e09033ad58a0e1875b8877303de3efd
toqurtl <39524978+toqurtl@users.noreply.github.com>
2020-02-12 18:42:03 +0900

    complete station model

--- modular_io/read_factory.py
+++ modular_io/read_factory.py
@@ -6,10 +6,9 @@ class temp_activity():
         self.name = str(line_list[1])
         self.manhour = int(line_list[2])
         self.labor_type = str(line_list[3])
-        self.fixed = bool(line_list[5])
-        self.num_labor = 0
-        if self.fixed:
-            self.num_labor = int(line_list[4])
+        self.labor_fixed = bool(line_list[5])
+        self.min_labor = int(line_list[4])
+        self.num_labor = int(line_list[4])
         self.location = str(line_list[6])
         self.max_labor = int(line_list[7])
 
@@ -23,10 +22,22 @@ def read_file(filename):
         for i, line in enumerate(lines):
             if i is not 0:
                 temp_list.append(line.split(','))
-    return temp_list
+    activity_list = []
+    for temp in temp_list:
+        temp_act = temp_activity(temp)
+        activity_list.append(Activity(temp_act=temp_act, num_labor=3))
+
+    return activity_list
 
 if __name__=='__main__':
     temp_list = read_file('../productionline.csv')
     activity_list = []
     for temp in temp_list:
-        activity_list.append(temp_activity(temp))
+        temp_act = temp_activity(temp)
+        activity_list.append(Activity(temp_act=temp_act, num_labor=3))
+
+    for act in activity_list:
+        act.print_act()
+
+
+
