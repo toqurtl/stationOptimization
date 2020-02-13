5e644cbc792d6e45c929c1f6b86da1edcdaeccbe
toqurtl <39524978+toqurtl@users.noreply.github.com>
2020-02-13 18:37:51 +0900

    refactoring factory model

---
+++ test.py
@@ -0,0 +1,31 @@
+import random
+import time
+import numpy as np
+from component.activity import Activity
+from component.station import Station
+from component.factory import Factory
+from component.production_line import ProductionLine
+
+from paretoGA.optimizer import Optimizer
+from functools import reduce
+from functools import partial
+from typing import List
+#
+
+production_line = ProductionLine('productionline.csv')
+temp_list = production_line.random_labor_num()
+# factory = Factory(cycle_time=140)
+# factory.build_factory(production_line=production_line, initialize=True)
+buildable, factory = production_line.create_factory(cycle_time=47, initialize=True)
+
+if buildable:
+    for station in factory:
+        station.print_station()
+        station.print_labor_map()
+    factory.simulate(10000)
+    print(factory.num_station, factory.num_unit, factory.num_labor)
+else:
+    print('fail to build factory, cycle time is too small')
+
+
+
