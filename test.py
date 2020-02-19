946fe653cf9d757979c438da2efe397b62ff0efb
toqurtl <39524978+toqurtl@users.noreply.github.com>
2020-02-19 18:06:54 +0900

    complete genetic algorithm, debugging

--- test.py
+++ test.py
@@ -5,19 +5,33 @@ from component.activity import Activity
 from component.station import Station
 from component.factory import Factory
 from component.production_line import ProductionLine
-
+from enum import Enum
 from paretoGA.optimizer import Optimizer
 from functools import reduce
 from functools import partial
 from typing import List
+from paretoGA.optimizer import Optimizer
+from paretoGA.generic import GenericEnum
 #
 #
-# production_line = ProductionLine('productionline.csv')
-# temp_list = production_line.random_labor_num()
+generic_ratio = {
+    GenericEnum.SUPERIOR: 0.3,
+    GenericEnum.SELECTION: 0.3,
+    GenericEnum.CROSSOVER: 0.2,
+    GenericEnum.LOCAL_MUTATION:0.1,
+    GenericEnum.GLOBAL_MUTATION:0.1
+}
+
+production_line = ProductionLine('productionline.csv')
+optimizer = Optimizer(production_line)
+optimizer.add_objective(1, 1)
+optimizer.add_objective(2, -1)
+optimizer.set_generic(generic_ratio)
+
 # factory = Factory(cycle_time=140)
 # factory.build_factory(production_line=production_line, initialize=True)
 # buildable, factory = production_line.create_factory(cycle_time=47, initialize=True)
-
+#
 # if buildable:
 #     for station in factory:
 #         station.print_station()
@@ -27,6 +41,5 @@ from typing import List
 #     factory.save('result.xlsx')
 # else:
 #     print('fail to build factory, cycle time is too small')
-test = [1,3,4,2,5]
-test.reverse()
-print(5/2)
+
+
