4a02ef27b0e60049906243c6df14b079628d5a38
toqurtl <39524978+toqurtl@users.noreply.github.com>
2020-04-28 12:22:39 +0900

    change gitignore

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
-import random
-import time
-import numpy as np
-from component.activity import Activity
-from component.station import Station
-from component.factory import Factory
-from paretoGA.generic import Generic
-from paretoGA.generation import Generation
-from paretoGA.front import Fronts
 from component.production_line import ProductionLine
-from enum import Enum
-from paretoGA.optimizer import Optimizer
-from functools import reduce
-from functools import partial
-from typing import List
 from paretoGA.optimizer import Optimizer
 from paretoGA.generic import GenericEnum
 from paretoGA.optimizer import ObjEnum
 from paretoGA.optimizer import OrderEnum
 import pickle
-#
+
 generic_ratio = {
     GenericEnum.SUPERIOR: 0,
     GenericEnum.SELECTION: 0,
@@ -45,26 +31,3 @@ optimizer.set_generic(generic_ratio, size=100, max_generation=10, simulation_tim
 optimizer.pareto_optimize()
 
 optimizer.save('final_result_initial.xlsx')
-
-
-
-# first_generation = Generation(production_line, 10000, 100, optimizer.generic)
-# first_generation.create_first_generation(optimizer)
-# first_generation.fronts = Fronts(first_generation, len(optimizer.objective_functions))
-# first_generation.fronts.save('front_result.xlsx')
-
-# factory = Factory(cycle_time=140)
-# factory.build_factory(production_line=production_line, initialize=True)
-# buildable, factory = production_line.create_factory(cycle_time=47, initialize=True)
-#
-# if buildable:
-#     for station in factory:
-#         station.print_station()
-#         station.print_labor_map()
-#     factory.simulate(10000)
-#     print(factory.num_station, factory.num_unit, factory.num_labor)
-#     factory.save('result.xlsx')
-# else:
-#     print('fail to build factory, cycle time is too small')
-
-
